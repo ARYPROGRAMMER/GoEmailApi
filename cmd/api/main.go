e93d0170af8b2ef72c87b0c699d947e8db49c80f
ARYPROGRAMMER <arya.2023ug1104@iiitranchi.ac.in>
2024-10-31 17:50:05 +0530

    db-established-final

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/ARYPROGRAMMER/GoEmailApi/internal/db"
 	"github.com/ARYPROGRAMMER/GoEmailApi/internal/env"
 	"github.com/ARYPROGRAMMER/GoEmailApi/internal/store"
 	"github.com/joho/godotenv"
@@ -13,11 +14,33 @@ func main(){
         log.Print("No .env file found")
     }
 
-	store := store.PostgresStorage(nil)
+
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr: env.GetString("DB_ADDR", "postgres://postgres:password@localhost/email?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+	
+	db,err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Print("Database connection pool established")
+
+	store := store.PostgresStorage(db)
 
 	app := &application{
 		config: cfg,
@@ -28,3 +51,4 @@ func main(){
 	mux :=app.mount()
 	log.Fatal(app.run(mux))
 }
+
